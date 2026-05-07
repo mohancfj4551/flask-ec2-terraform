ae6df1fd4357024d1f75907ebe263c5e6b30d66d
mohancfj4551 <mohan.desappan@agilisium.com>
2026-05-07 15:16:58 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "🚀 Flask App running on Amazon Linux (us-west-2)!"
+    return "🚀 Flask App running on Amazon Linux (us-west-2)! Done by Mohan D"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
